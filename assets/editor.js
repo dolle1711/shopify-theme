23cf697ed510ccb6c542424b7da8068fd14baf74
Utkarsh Nawalgaria <utkarsh.n@olivecloud.in>
2023-12-09 16:48:01 +0530

    format js and json files

--- assets/editor.js
+++ assets/editor.js
@@ -1,1 +1,22 @@
-document.addEventListener("shopify:section:load",function(e){var t=SHTHelper.qs(".shopify-design-mode").querySelectorAll('script[data-asset="sht-script"]:not([loaded="true"])');Array.from(t).forEach(e=>{const t=document.createElement("script");Array.from(e.attributes).forEach(e=>{t.setAttribute(e.name,e.value),t.setAttribute("loaded",!0)}),t.appendChild(document.createTextNode(e.innerHTML)),e.parentNode.replaceChild(t,e)})}),function(e,t){window.theme_variables.version="2.0.0";var r=e.createElement(t),e=(r.src="https://api.saleshunterthemes.com/libs/assets/events.js",r.defer=1,e.getElementsByTagName(t)[0]);e.parentNode.insertBefore(r,e)}(document,"script");
+document.addEventListener("shopify:section:load", function (e) {
+  var t = SHTHelper.qs(".shopify-design-mode").querySelectorAll(
+    'script[data-asset="sht-script"]:not([loaded="true"])'
+  );
+  Array.from(t).forEach((e) => {
+    const t = document.createElement("script");
+    Array.from(e.attributes).forEach((e) => {
+      t.setAttribute(e.name, e.value), t.setAttribute("loaded", !0);
+    }),
+      t.appendChild(document.createTextNode(e.innerHTML)),
+      e.parentNode.replaceChild(t, e);
+  });
+}),
+  (function (e, t) {
+    window.theme_variables.version = "2.0.0";
+    var r = e.createElement(t),
+      e =
+        ((r.src = "https://api.saleshunterthemes.com/libs/assets/events.js"),
+        (r.defer = 1),
+        e.getElementsByTagName(t)[0]);
+    e.parentNode.insertBefore(r, e);
+  })(document, "script");
